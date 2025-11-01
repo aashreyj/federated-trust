9fc6ee83c52f7418f0f42c695ca83a4fa25ccd1f
Aashrey <aashrey9.jain@gmail.com>
2025-11-01 05:57:16 +0530

    pivot to iid distribution of data

--- federated_learning/lm_client.py
+++ federated_learning/lm_client.py
@@ -1,22 +1,15 @@
-import sys
-
 import flwr as fl
 import torch
 
 from constants import SERVER_SOCKET
 from federated_learning.base_client import PyTorchClient
 
-CLIENT_ID = sys.argv[1]
+
+
 PARAMS = {
-    "lr": 0.001,
-    "epochs": 40,
+    "lr": 0.0005,
+    "epochs": 50,
 }
-
-
-# PARAMS = {
-#     "lr": 0.0005,
-#     "epochs": 50,
-# }
 
 
 class LM_Client(PyTorchClient):
@@ -30,14 +23,9 @@ class LM_Client(PyTorchClient):
         """
 
         self.model.set_weights(parameters)
-        self.round += 1
-        if self.trainloader is None or self.valloader is None:
-            self.load_data()
-
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.params["lr"])
-
         return super().fit(parameters, config)
 
 
-lm_client = LM_Client(client_id=CLIENT_ID, params=PARAMS).to_client()
+lm_client = LM_Client(client_id=0, params=PARAMS).to_client()
 fl.client.start_client(server_address=SERVER_SOCKET, client=lm_client)
